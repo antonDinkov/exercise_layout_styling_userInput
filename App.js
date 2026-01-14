1f028e8da3a8c0d6b6d85569abbb6d9ff2f0bbfd
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-01-14 11:13:04 +0700

    discussion forum initial commit

--- App.js
+++ App.js
@@ -2,19 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <View style={styles.container}>
+                <StatusBar style="auto" />
+                <Text style={{fontSize: 20, fontWeight: 'bold', color: 'orange'}}>Flippin egg - Discussion Forum</Text>
+            </View>
+            <View>
+                <Text>Topics:</Text>
+            </View>
+        </>
+
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        marginTop: 40,
+    },
 });
